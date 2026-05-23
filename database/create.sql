f616b2c98b9c9798401794215793f088907f1c3a
Spencer Siu <spencersiu02@gmail.com>
2026-05-23 20:14:38 +0930

    Slight Revise with comments on DB

--- database/create.sql
+++ database/create.sql
@@ -6,34 +6,41 @@ DROP TABLE IF EXISTS Products;
 DROP TABLE IF EXISTS Customers;
 
 CREATE TABLE Customers(
-    CustomerID INT AUTO_INCREMENT,
+    CustomerID INT NOT NULL AUTO_INCREMENT, -- added NOT NULL since its a PK
     Email VARCHAR(255) NOT NULL,
     FirstName VARCHAR(50) NOT NULL,
     LastName VARCHAR(50) NOT NULL,
     Title ENUM("Mr.","Mrs.","Mx.","Ms."),
-    Address VARCHAR(50) NOT NULL,
-    City VARCHAR(25) NOT NULL,
-    State VARCHAR(25) NOT NULL,
-    Country VARCHAR(25) NOT NULL,
-    PostCode INT(4) NOT NULL,
-    Phone VARCHAR(12) NOT NULL,
-    Admin BOOL NOT NULL,
+    Address VARCHAR(75) NOT NULL, -- increased address length
+    City VARCHAR(50) NOT NULL,
+    State VARCHAR(50) NOT NULL,
+    Country VARCHAR(50) NOT NULL,
+    PostCode VARCHAR(10) NOT NULL, -- Changed to VARCHAR since leading zeroes might get stripped
+    Phone VARCHAR(15) NOT NULL, -- Increased size, since area code can increase it or spaces can take an index
+
     PRIMARY KEY (CustomerID)
+    -- remove customer logins. Purchasing order no need for --> Email details etc......
+    -- I Removed the admin bool as i think we go ahead with no user accounts.
 );
 
 CREATE TABLE Products(
-    ProductID INT AUTO_INCREMENT,
-    Description VARCHAR(50) NOT NULL,
-    Price INT(9) NOT NULL,
-    Category ENUM("Painting","Sculpture","Drawing"),
+    ProductID INT NOT NULL AUTO_INCREMENT,
+    Title VARCHAR(100) NOT NULL, -- added title so we dont need to put it in the description also
+    Description TEXT NOT NULL,
+    Price DECIMAL(10,2) NOT NULL, -- changed from INT to decimal since price can be a float and money generally is like $100.99
+    Category ENUM("Painting","Sculpture","Drawing") NOT NULL,
+    Image VARCHAR(255) DEFAULT 'placeholder.jpg', -- image so we can store img file, put a placeholder in case of empty entry [not img binary data]
+
     PRIMARY KEY (ProductID)
 );
 
 CREATE TABLE Purchases(
-    PurchaseID INT AUTO_INCREMENT,
-    Date DATETIME NOT NULL,
-    CustomerID INT,
+    PurchaseID INT NOT NULL AUTO_INCREMENT,
+    CustomerID INT NOT NULL, -- added NOT NULL
+    PurchaseDate DATETIME NOT NULL, -- altered attribute to match entity
+    
     PRIMARY KEY (PurchaseID),
+
     CONSTRAINT fk_Customer
     FOREIGN KEY (CustomerID)
     REFERENCES Customers(CustomerID)
@@ -44,9 +51,11 @@ CREATE TABLE PurchaseItem(
     ProductID INT NOT NULL,
     Quantity INT(5) NOT NULL,
     PRIMARY KEY (PurchaseID, ProductID),
+
     CONSTRAINT fk_Purchase
     FOREIGN KEY (PurchaseID)
     REFERENCES Purchases(PurchaseID),
+
     CONSTRAINT fk_Product
     FOREIGN KEY (ProductID)
     REFERENCES Products(ProductID)
